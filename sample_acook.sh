f72ca17296808bcbe88d81c698144feef2630405
tryingtobeacook <107980914+tryingtobeacook@users.noreply.github.com>
2023-09-05 18:25:02 +0530

    Update sample_acook.sh

--- sample_acook.sh
+++ sample_acook.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SQLPLUS_PATH="/opt/oracle/instantclient_21_4/sqlplus"
+export LD_LIBRARY_PATH="/opt/oracle/instantclient_21_4"
 
 # Define Oracle database connection details
 DB_USER="your_username"
