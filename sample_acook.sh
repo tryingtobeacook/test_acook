426397cbcb4e0b7db51ce5313956469cbf17eb26
tryingtobeacook <107980914+tryingtobeacook@users.noreply.github.com>
2023-09-05 18:17:17 +0530

    Create sample_acook.sh

---
+++ sample_acook.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define Oracle database connection details
+DB_USER="your_username"
+DB_PASS="your_password"
+DB_HOST="your_host"
+DB_PORT="your_port"
+DB_SID="your_sid"
+
+# SQL query to execute
+SQL_QUERY="SELECT * FROM your_table"
+
+# Execute SQLPlus command
+sqlplus --version
+
