b5eaf0200d680c732028e5a01b979fa7c649a7f7
tryingtobeacook <107980914+tryingtobeacook@users.noreply.github.com>
2023-09-06 14:55:22 +0530

    acook5

--- sample_acook.sh
+++ sample_acook.sh
@@ -2,8 +2,8 @@
 
 #SQLPLUS_PATH="/opt/oracle/instantclient_21_4/sqlplus"
 #export LD_LIBRARY_PATH="/opt/oracle/instantclient_21_4"
-export ORACLE_HOME=/opt/oracle/instantclient_19_11
-export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
+# export ORACLE_HOME=/opt/oracle/instantclient_19_11
+# export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
 
 
 # Define Oracle database connection details
