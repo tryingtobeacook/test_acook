eb8130cbe94b52eb097347fd69fc716c3a87ced8
tryingtobeacook <107980914+tryingtobeacook@users.noreply.github.com>
2023-09-06 21:16:19 +0530

    made small changes to sample_acook.sh

--- sample_acook.sh
+++ sample_acook.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#SQLPLUS_PATH="/opt/oracle/instantclient_21_4/sqlplus"
-#export LD_LIBRARY_PATH="/opt/oracle/instantclient_21_4"
-# export ORACLE_HOME=/opt/oracle/instantclient_19_11
-# export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
-
 
 # Define Oracle database connection details
 DB_USER="your_username"
@@ -18,4 +13,7 @@ SQL_QUERY="SELECT * FROM your_table"
 # Execute SQLPlus command
 sqlplus -V
 
-sqlplus $DB_USER/$DB_PASS@$DB_HOST
+sqlplus $DB_USER/$DB_PASS@$DB_HOST <<EOF
+$SQL_QUERY
+exit
+EOF
